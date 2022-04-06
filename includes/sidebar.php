213a2bedd577ce2e4845fdf0003ffe7e2098cd96
judvelasco29 <201980205@psu.palawan.edu.ph>
2022-04-06 21:15:38 +0800

    includes added

---
+++ includes/sidebar.php
@@ -0,0 +1,31 @@
+<div class="logo">
+                <a href="javascript:;" class="simple-text">
+                    Your logo
+                </a>
+            </div>
+            <ul class="nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="dashboard.html">
+                        <i class="nc-icon nc-icon nc-paper-2"></i>
+                        <p>Transaction</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./user.html">
+                        <i class="nc-icon nc-bank"></i>
+                        <p>Office</p>
+                    </a>
+                </li>
+                 <li>
+                    <a class="nav-link" href="./user.html">
+                        <i class="nc-icon nc-alien-33"></i>
+                        <p>Employee</p>
+                    </a>
+                </li>
+
+                <li class="nav-item active active-pro">
+                    <a class="nav-link active" href="javascript:;">
+                        <i class="nc-icon nc-alien-33"></i>
+                        <p>Upgrade Pla</p>
+                    </a>
+                </li>
